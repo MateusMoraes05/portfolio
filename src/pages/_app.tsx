9558d396d9db558bc062ea5ae8bf020599ffcadc
Mateus Moraes de Oliveira <teus@MacBook-Pro-de-Mateus.local>
2023-02-03 20:18:20 -0300

    primeiro commit - navbar em desenvolvimento

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
+import { JetBrains_Mono } from '@next/font/google'
 import type { AppProps } from 'next/app'
 
+
+const jetBrains_mono = JetBrains_Mono({ 
+  subsets: ['latin'],
+  weight: ['200', '400', '800']
+})
+
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <main className={jetBrains_mono.className}>
+    <Component {...pageProps} />
+  </main>
+  )
 }
